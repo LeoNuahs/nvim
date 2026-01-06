d7bf0d5573912f46255aece8127b7e50dab2cadd
Shaun <leonuahs@gmail.com>
2026-01-06 11:23:20 +0800

    feat: Disable linting for markdown

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "mfussenegger/nvim-lint",
+        opts = {
+            linters_by_ft = {
+                -- Explicitly set to an empty table to disable all linters
+                markdown = {},
+            },
+        },
+    },
+}
