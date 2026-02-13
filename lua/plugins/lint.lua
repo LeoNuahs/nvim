4fe5a5b6eb129b6c2a27a5731e3968f292f46bbc
Shaun <leonuahs@gmail.com>
2026-02-13 11:20:36 +0800

    feat: Add checkstyle for OnB

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -5,6 +5,15 @@ return {
             linters_by_ft = {
                 -- Explicitly set to an empty table to disable all linters
                 markdown = {},
+                java = { "checkstyle" },
+            },
+            linters = {
+                checkstyle = {
+                    -- Pass the path to your checkstyle.xml here
+                    args = {
+                        "-f", "sarif", "-c", vim.fn.expand("~/.config/nvim/formats/java/onb-checkstyle.xml")
+                    },
+                },
             },
         },
     },
